c1b6c49d1155d46f073419f250dc80586af45081
yury <yury.market.ma@gmail.com>
2023-06-10 23:25:49 +0200

    add rds dev

--- terraform/modules/aws/vpc/main.tf
+++ terraform/modules/aws/vpc/main.tf
@@ -3,7 +3,7 @@ resource "aws_vpc" "kuber" {
   instance_tenancy = "default"
 
   tags = {
-    Name = "dev"
+    Name = "kuber"
   }
 }
 
@@ -15,7 +15,7 @@ resource "aws_subnet" "kuber_1a" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "front"
+    Name = "kuber"
   }
 }
 
@@ -26,14 +26,14 @@ resource "aws_subnet" "kuber_1b" {
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "front"
+    Name = "kuber"
   }
 }
 
 
 resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls_dev"
-  description = "Allow TLS inbound traffic in dev environment"
+  name        = "allow_tls_kuber"
+  description = "Allow TLS inbound traffic in kuber"
   vpc_id      = aws_vpc.kuber.id
 
   dynamic "ingress" {
@@ -43,7 +43,6 @@ resource "aws_security_group" "allow_tls" {
       to_port     = ingress.value
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
-      #   ipv6_cidr_blocks = [aws_vpc.dev.ipv6_cidr_block]
     }
   }
 
@@ -56,7 +55,7 @@ resource "aws_security_group" "allow_tls" {
   }
 
   tags = {
-    Name = "allow_tls"
+    Name = "allow_tls_kuber"
   }
 }
 
@@ -66,7 +65,7 @@ resource "aws_internet_gateway" "gw_kuber" {
   vpc_id = aws_vpc.kuber.id
 
   tags = {
-    Name = "gw_dev"
+    Name = "gw_kuber"
   }
 }
 
@@ -81,7 +80,7 @@ resource "aws_nat_gateway" "nat_gw_kuber" {
   allocation_id = aws_eip.nat_gw_kuber_eip.allocation_id
 
   tags = {
-    Name = "gw_NAT_dev"
+    Name = "gw_NAT_kuber"
   }
   depends_on = [aws_eip.nat_gw_kuber_eip]
 }
@@ -95,7 +94,7 @@ resource "aws_route_table" "kuber_1a" {
   }
 
   tags = {
-    Name = "frontend-route-table"
+    Name = "kuber_1a-route-table"
   }
 }
 
@@ -108,7 +107,7 @@ resource "aws_route_table" "kuber_1b" {
   }
 
   tags = {
-    Name = "backend-route-table"
+    Name = "kuber_1b-route-table"
   }
 }
 
@@ -123,6 +122,174 @@ resource "aws_route_table_association" "kuber_1b" {
 }
 
 
+resource "aws_vpc" "rds_dev" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
+  tags = {
+    Name = "rds_dev"
+  }
+}
+
+resource "aws_subnet" "rds_dev_1a" {
+  vpc_id                  = aws_vpc.rds_dev.id
+  cidr_block              = "10.0.4.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "rds_dev_1a"
+  }
+}
+
+
+resource "aws_security_group" "rds_dev_1a" {
+  name   = "rds_dev_1a"
+  vpc_id = aws_vpc.rds_dev.id
+
+  dynamic "ingress" {
+    for_each = ["22", "80", "443", "8080", "5432"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+
+    }
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "rds_dev_1a"
+  }
+}
+
+
+
+resource "aws_internet_gateway" "gw_rds_dev_1a" {
+  vpc_id = aws_vpc.rds_dev.id
+
+  tags = {
+    Name = "gw_rds_dev_1a"
+  }
+}
+
+
+resource "aws_eip" "nat_gw_rds_dev_1a_eip" {
+  vpc = true
+}
+
+
+resource "aws_nat_gateway" "nat_gw_rds_dev_1a" {
+  subnet_id     = aws_subnet.rds_dev_1a.id
+  allocation_id = aws_eip.nat_gw_rds_dev_1a_eip.allocation_id
+
+  tags = {
+    Name = "gw_NAT_rds_dev_1a"
+  }
+  depends_on = [aws_eip.nat_gw_rds_dev_1a_eip]
+}
+
+resource "aws_route_table" "rds_dev_1a" {
+  vpc_id = aws_vpc.rds_dev.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_rds_dev_1a.id
+  }
+
+  tags = {
+    Name = "rds_dev_1a-route-table"
+  }
+}
+
+
+resource "aws_route_table_association" "rds_dev_1a" {
+  subnet_id      = aws_subnet.rds_dev_1a.id
+  route_table_id = aws_route_table.rds_dev_1a.id
+}
+
+
+
+resource "aws_subnet" "rds_dev_1b" {
+  vpc_id                  = aws_vpc.rds_dev.id
+  cidr_block              = "10.0.5.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "rds_dev_1b"
+  }
+}
+
+
+resource "aws_security_group" "rds_dev_1b" {
+  name   = "rds_dev_1b"
+  vpc_id = aws_vpc.rds_dev.id
+
+  dynamic "ingress" {
+    for_each = ["22", "80", "443", "8080", "5432"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+
+    }
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "rds_dev_1b"
+  }
+}
+
+resource "aws_eip" "nat_gw_rds_dev_1b_eip" {
+  vpc = true
+}
+
+
+resource "aws_nat_gateway" "nat_gw_rds_dev_1b" {
+  subnet_id     = aws_subnet.rds_dev_1b.id
+  allocation_id = aws_eip.nat_gw_rds_dev_1b_eip.allocation_id
+
+  tags = {
+    Name = "gw_NAT_rds_dev_1b"
+  }
+  depends_on = [aws_eip.nat_gw_rds_dev_1b_eip]
+}
+
+resource "aws_route_table" "rds_dev_1b" {
+  vpc_id = aws_vpc.rds_dev.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_rds_dev_1a.id
+  }
+
+  tags = {
+    Name = "rds_dev_1b-route-table"
+  }
+}
+
+
+resource "aws_route_table_association" "rds_dev_1b" {
+  subnet_id      = aws_subnet.rds_dev_1b.id
+  route_table_id = aws_route_table.rds_dev_1b.id
+}
 
 
